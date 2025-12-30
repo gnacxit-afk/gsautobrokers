4e49f12ef02ac51fb9ee0c73b697fa704589dc7f
Nacxit Gomez <gnacxit@gmail.com>
2025-12-30 17:27:44 +0000

    use this one instead

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -2,5 +2,5 @@ import Image from 'next/image';
 import type { ComponentProps } from 'react';
 
 export function Logo(props: Partial<ComponentProps<typeof Image>>) {
-  return <Image src="/logo.png" alt="GS Auto Brokers Logo" width={150} height={35} {...props} />;
+  return <Image src="https://storage.googleapis.com/stabl-media/69796792-2621-4a57-96a1-6a59600d8977.png" alt="GS Auto Brokers Logo" width={150} height={35} {...props} />;
 }
