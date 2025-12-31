7b9876be687f38db81d0cebbf6b2981ce6fc4d81
Nacxit Gomez <gnacxit@gmail.com>
2025-12-31 20:23:51 +0000

    add the email gnacxit@gmail.com and password annagcexlit.5691 as super a

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,8 +12,8 @@ import { Logo } from "@/components/icons";
 
 export default function LoginPage() {
   const { login } = useAuth();
-  const [email, setEmail] = useState("ava.j@gsautobrokers.com");
-  const [password, setPassword] = useState("password123");
+  const [email, setEmail] = useState("gnacxit@gmail.com");
+  const [password, setPassword] = useState("annagcexlit.5691");
   const [loading, setLoading] = useState(false);
   const { toast } = useToast();
 
