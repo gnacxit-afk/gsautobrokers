945eab77b4f4b70244093fe7b839b0fd66be5d96
Nacxit Gomez <gnacxit@gmail.com>
2025-12-31 20:22:03 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,11 @@
+
 // This file is intentionally left blank. 
 // The Firebase configuration will be populated by the Firebase service.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  "projectId": "studio-7674486159-b9551",
+  "appId": "1:1011351605710:web:30e1de9b250e21d25edb9f",
+  "apiKey": "AIzaSyDqGBvYbqI8-PG_qvEZBTq05fyMk3oyO7c",
+  "authDomain": "studio-7674486159-b9551.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1011351605710"
+};
