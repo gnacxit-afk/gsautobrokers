b7e0b042e1906f01e1842c45d97e41968ab883c5
Nacxit Gomez <gnacxit@gmail.com>
2026-01-14 08:10:14 +0000

    ## Error Type Runtime Error
    ## Error Message
    useDateRange must be used

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,17 @@
+
 "use client";
 
 import { FirebaseProvider } from "@/firebase/provider";
 import { AuthProvider } from "@/lib/auth";
+import { DateRangeProvider } from "@/providers/date-range-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <FirebaseProvider>
       <AuthProvider>
-        {children}
+        <DateRangeProvider>
+          {children}
+        </DateRangeProvider>
       </AuthProvider>
     </FirebaseProvider>
   );
