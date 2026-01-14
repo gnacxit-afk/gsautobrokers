63efd2432ab5a5ee70cf192b26faf1671e249288
Nacxit Gomez <gnacxit@gmail.com>
2026-01-14 14:31:14 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,12 @@
 import type { FirebaseOptions } from 'firebase/app';
 
 export const firebaseConfig: FirebaseOptions = {
-  projectId: "studio-7674486159-b9551",
-  appId: "1:1011351605710:web:30e1de9b250e21d25edb9f",
-  apiKey: "AIzaSyDqGBvYbqI8-PG_qvEZBTq05fyMk3oyO7c",
-  authDomain: "studio-7674486159-b9551.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "1011351605710"
+  apiKey: "AIzaSyBE77gLf2veDaSXXuOz33Cr4ubQyoMRSOQ",
+  authDomain: "gs-auto-brokers.firebaseapp.com",
+  databaseURL: "https://gs-auto-brokers-default-rtdb.firebaseio.com",
+  projectId: "gs-auto-brokers",
+  storageBucket: "gs-auto-brokers.firebasestorage.app",
+  messagingSenderId: "423258761446",
+  appId: "1:423258761446:web:fed9d2fafe1199720db327",
+  measurementId: "G-LTK7VF563H"
 };
