a2cf8aa9a0c996a55e742e39c2e3755ee9752d1f
Nacxit Gomez <gnacxit@gmail.com>
2026-01-15 19:51:35 +0000

    restaura el respositorio studio

--- src/app/(public)/layout.tsx
+++
@@ -1,6 +0,0 @@
-
-// This layout is intentionally simple. It does not include any authentication providers.
-// It's used for pages that should be accessible to everyone, like the application form.
-export default function PublicLayout({ children }: { children: React.ReactNode }) {
-    return <>{children}</>;
-}
