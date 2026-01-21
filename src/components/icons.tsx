dfec68261777f765fcfb0295ccc29fd8c5526646
Nacxit Gomez <gnacxit@gmail.com>
2026-01-21 06:11:06 +0000

    https://imgur.com/a/Pg53LPN

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -14,7 +14,7 @@ export function Logo({ className, width = 180, height = 50, ...props }: { classN
       {/* Reverted to a standard <img> tag to bypass any Next.js image optimization issues
           and ensure the logo is displayed directly from the source URL. */}
       <img 
-        src="https://drive.google.com/uc?export=view&id=1kF35eGHp-Rp1NegG0gSqjkmlQnXFbwer"
+        src="https://i.imgur.com/k6wTvyy.png"
         alt="GS Autobrokers Logo"
         width={width}
         height={height}
